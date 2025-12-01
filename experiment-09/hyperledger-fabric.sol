33c6f43f2d9ccd89c3de87dacea016b5f473e43e
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-12-01 21:40:08 +0530

    Update hyperledger-fabric.sol

--- experiment-09/hyperledger-fabric.sol
+++ experiment-09/hyperledger-fabric.sol
@@ -1,21 +1,21 @@
-# 1) Download the fabric
+1) Download the fabric
 curl -sSL https://bit.ly/2ysb0FE | bash -s
 
-# 2) Navigate to test networks
+2) Navigate to test networks
 cd fabric-samples/test_network
 
-# 3) Start the network
+3) Start the network
 sudo ./network.sh up
 
-# 4) Check for running channels
+4) Check for running channels
 sudo docker exec peer0.org1.example.com peer channel list
 
-# 5) Create a test channel
+5) Create a test channel
 sudo ./network.sh createChannel -c testChannel
 
-# 6) Join two channels
+6) Join two channels
 sudo docker exec peer0.org1.example.com peer channel list
 sudo docker exec peer0.org2.example.com peer channel list
 
-# 7) Close the channel
+7) Close the channel
 sudo ./network.sh down
