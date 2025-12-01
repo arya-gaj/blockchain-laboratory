93b4db5cb6b26676b1b611ce178fe1417df0af9d
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-12-01 21:03:03 +0530

    Update array.sol

--- experiment-07/array.sol
+++ experiment-07/array.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Arraylength {
+    uint[] public integers;
+
+    constructor() {
+        integers = [1, 2, 3, 4, 5];
+    }
+
+    function getArray() public view returns (uint[] memory) {
+        return integers;
+    }
+
+    function getArrayLength() public view returns (uint) {
+        return integers.length;
+    }
+}
