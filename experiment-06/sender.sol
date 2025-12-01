828cf5d1af58545dfea3eb8b9832523be73b65e6
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-12-01 20:45:32 +0530

    Update sender.sol

--- experiment-06/sender.sol
+++ experiment-06/sender.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Sender {
+    address public owner;
+    
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function sendEther(address payable _to) public payable {
+        require(msg.value > 0, "Send some Ether");
+        require(_to != address(0), "Invalid address");
+        _to.transfer(msg.value);
+    }
+}
