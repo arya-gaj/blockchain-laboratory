a7a2705139368a71828b9a7316313b880c702838
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-12-01 20:45:58 +0530

    Update receiver.sol

--- experiment-06/receiver.sol
+++ experiment-06/receiver.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Receiver {
+    uint public balance;
+
+    // Receive Ether
+    receive() external payable {
+        balance += msg.value;
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
