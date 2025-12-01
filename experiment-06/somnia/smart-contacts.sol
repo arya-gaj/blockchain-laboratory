8f9405cd324543a3529940a4e3fedf65dd4d659f
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-12-01 21:36:36 +0530

    Update smart-contacts.sol

--- experiment-06/somnia/smart-contacts.sol
+++ experiment-06/somnia/smart-contacts.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+interface Token {
+    function transfer(address to, uint256 amount) external returns (bool);
+}
+
+contract SendToken {
+    function send(address token, address to, uint256 amount) external {
+        Token(token).transfer(to, amount);
+    }
+}
