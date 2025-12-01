4467c8dc324da8f9894acfb1aad49ea4f2e06af1
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-12-01 22:47:04 +0530

    Update variables.sol

--- experiment-10/variables.sol
+++ experiment-10/variables.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.1;
 
+contract Variables {
+    string public stateVariable = "This is a state variable";
+
+    function fun() public view returns (string memory) {
+        string memory localVariable = "This is a Local Variable";
+        return localVariable;
+    }
+}
