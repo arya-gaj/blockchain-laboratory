dc2dd8ade0e565012d5fc352a02ccf3d8c315df3
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-12-01 22:47:47 +0530

    Update operators.sol

--- experiment-10/operators.sol
+++ experiment-10/operators.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.1;
 
+contract Operators {
+
+    function multiply(uint8 a, uint8 b) public pure returns (uint8) {
+        return a * b;
+    }
+
+    function compare(uint8 a, uint8 b, uint8 c) public pure returns (uint8) {
+        uint8 largest = a;
+
+        if (a > b) {
+            if (a - c < 0)
+                largest = c;
+        } 
+        else if (b > c) {
+            largest = b;
+        } 
+        else {
+            largest = c;
+        }
+
+        return largest;
+    }
+}
