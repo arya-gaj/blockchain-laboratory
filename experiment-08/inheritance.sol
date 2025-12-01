5c352de7a5db4e633a3ef9686a2ca9feb5b6bb87
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-12-01 21:14:38 +0530

    Update inheritance.sol

--- experiment-08/inheritance.sol
+++ experiment-08/inheritance.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Shape {
+    string internal shapeType;
+    constructor(string memory t){ shapeType = t; }
+    function getType() public view returns(string memory){ return shapeType; }
+}
+
+contract Rectangle is Shape {
+    uint w; uint h;
+    constructor(uint _w, uint _h) Shape("Rectangle"){ w=_w; h=_h; }
+    function getArea() public view returns(uint){ return w*h; }
+}
+
+contract Circle is Shape {
+    uint r;
+    constructor(uint _r) Shape("Circle"){ r=_r; }
+    function getArea() public view returns(uint){ return (3 * r * r) / 4; }
+}
